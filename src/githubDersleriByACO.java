cb99d1241bc2df7608eb03e40608fac6531d468c
sinantalhaincesu <snn99tlh@hotmail.com>
2023-12-07 18:52:32 +0300

    second commit/added second hello world

--- src/githubDersleriByACO.java
+++ src/githubDersleriByACO.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class githubDersleriByACO {
+public class githubDersleriByACO {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!123");
+    }
 }
